ae3d7a441907c312d085cba8eb95c74e95d01d57
Esdras Carneiro <esdras.aslc2@gmail.com>
2022-05-19 17:27:23 -0300

    initial commit

---
+++ correcao.js
@@ -0,0 +1,36 @@
+function correcao () {
+
+    let corretas = 0;
+    let erradas = 0;
+
+    const q2anwer = document.querySelector('#q2-a');
+    const q3anwer = document.querySelector('#q3-d');
+    const q4anwer = document.querySelector('#q4-b');
+
+    if (q2anwer.checked) {
+        corretas++
+    } else {
+        erradas++
+    } 
+
+    if (q3anwer.checked) {
+        corretas++
+    } else {
+        erradas++
+    } 
+
+    if (q4anwer.checked) {
+        corretas++
+    } else {
+        erradas++
+    } 
+
+    // if (q5anwer.checked) {
+    //     corretas++
+    // } else {
+    //     erradas++
+    // } 
+
+
+    
+}
